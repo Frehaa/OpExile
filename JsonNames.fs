caafa8f43b08b0854860eb48f57473db75b2cc9f
Frederik Haagensen <haag@itu.dk>
2018-05-26 22:48:05 +0200

    More parsing

---
+++ JsonNames.fs
@@ -0,0 +1,22 @@
+module Names
+
+[<Literal>]
+let Scion = "0"
+
+[<Literal>]
+let Marauder = "1"
+
+[<Literal>]
+let Ranger = "2"
+
+[<Literal>]
+let Witch = "3"
+
+[<Literal>]
+let Duelist = "4"
+
+[<Literal>]
+let Templer = "5"
+
+[<Literal>]
+let Shadow = "6"
--- SkillTreeExtractor.fs
+++ SkillTreeExtractor.fs
@@ -1,9 +1,27 @@
+open System.Net
+open System
+
+let loadContent (address: String) =
+    use client = new WebClient()   
+    client.DownloadString address
+
+let capture (captureStart: String) (content: String) = 
+    let startIdx = content.IndexOf(captureStart) + captureStart.Length
+    let endIdx = content.IndexOf(";", startIdx)    
+    content.Substring(startIdx, endIdx - startIdx)
+
+let captureTreeData = capture @"var passiveSkillTreeData = "
+let captureOptions = capture @"var opts = "   
 
 [<EntryPoint>]
 let main args = 
     let path = @"https://www.pathofexile.com/passive-skill-tree"
 
-    printfn "%A" path
+    let content = loadContent path
+    
+    captureOptions content
+    |> printfn "%s"
+
     0
 
     
